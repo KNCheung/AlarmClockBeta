e6ac4819508f010944c007b39b099fcb1abe5547
Troy <TroyC.SNS@gmail.com>
2014-04-22 10:08:14 +0800

    add power display at the begining

--- Software-RTT/bsp/stm32f10x/applications/task_protect.c
+++ Software-RTT/bsp/stm32f10x/applications/task_protect.c
@@ -12,9 +12,13 @@ uint8_t voltage;
 void rt_thread_protect_entry(void* parameter)
 {
   rt_uint32_t e;
+  voltage = GetVoltage();
+  rt_event_send(reg_event,REG_PROTECT_MSK);
+  reg_output[REG_PROTECT]=REG_Convert(0x63,REG_HexToReg(voltage%10),REG_HexToReg(voltage/10%10),REG_HexToReg(voltage/100%10),0,1);
+  rt_thread_delay(RT_TICK_PER_SECOND*1);
+  rt_event_recv(reg_event,REG_PROTECT_MSK,RT_EVENT_FLAG_OR|RT_EVENT_FLAG_CLEAR,0,&e);
   while (1)
   {
-    voltage = GetVoltage();
     if (voltage<=108)
     {
       while (1)
